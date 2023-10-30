1be2c08c4a0f3248f5b0d84693b0e7c6731dae09
Wenyu Ye <wenyuye@bupt.edu.cn>
2023-10-30 06:04:05 +0000

    parser --let  to be tested and completed

--- parser/parser.go
+++ parser/parser.go
@@ -1,19 +1,19 @@
 package parser
 
-import(
+import (
 	"awesomeDSL/ast"
 	"awesomeDSL/lexer"
 	"awesomeDSL/token"
 )
 
-type Parser struct{
+type Parser struct {
 	l *lexer.Lexer
 
-	curToken token.Token
+	curToken  token.Token
 	peekToken token.Token
 }
 
-func New(l *lexer.Lexer) *Parser{
+func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l}
 
 	//init cur&peek Token
@@ -23,8 +23,61 @@ func New(l *lexer.Lexer) *Parser{
 	return p
 }
 
-func (p *Parser)NextToken(){
-	p.curToken=p.peekToken
-	p.peekToken=p.l.NextToken()
+func (p *Parser) NextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
 }
 
+func (p *Parser)phraseStatement() ast.Statement{
+	switch p.curToken.Type{
+	case token.LET:
+		return p.phraseLetStatement()
+	default:
+		return nil
+	}
+}
+func (p *Parser)phraseLetStatement() ast.Statement{
+	stmt:=&ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.ASSIGN){
+		return nil
+	}
+
+	//Todo:expresion here
+	for !p.curTokenIs(token.SEMICOLON){
+		p.NextToken()
+	}
+	return stmt
+}
+
+
+func (p *Parser) curTokenIs(t token.TokenType) bool{
+	return p.curToken.Type==t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool{
+	return p.peekToken.Type==t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool{
+	if p.peekTokenIs(t){
+		p.NextToken()
+		return true
+	}else {
+		return false
+	}
+}
+
+func (p *Parser) PhraseProgram() *ast.Program {
+	program:=&ast.Program{}
+	program.Statements=[]ast.Statement{}
+
+	for p.curToken.Type!=token.EOF{
+		stmt:=p.phraseStatement()
+		if stmt!=nil{
+			program.Statements=append(program.Statements, stmt)
+		}
+		p.NextToken()
+	}
+	return program
+}
