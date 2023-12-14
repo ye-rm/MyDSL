b80ee4b5a7a323ba9778485d34af91c6035f28a7
wenyuye@bupt.edu.cn <wenyuye@bupt.edu.cn>
2023-12-14 23:54:53 +0800

    now awesome support tui

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"awesomeDSL/repl"
-	"fmt"
-	"os"
-	"os/user"
+	"awesomeDSL/tui"
 )
 
-func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
+//func main() {
+//	user, err := user.Current()
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	fmt.Printf("Hello %s! This is awesomeDSL to help you build your chatting bot\n", user.Username)
+//	fmt.Print("Please type in commands\n")
+//	repl.Start(os.Stdin, os.Stdout)
+//}
 
-	fmt.Printf("Hello %s! This is awesomeDSL to help you build your chatting bot\n", user.Username)
-	fmt.Print("Please type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+func main() {
+	tui.GUI()
 }
--- tui/tui.go
+++ tui/tui.go
@@ -1,1 +1,133 @@
 package tui
+
+import (
+	"awesomeDSL/evaluator"
+	"awesomeDSL/lexer"
+	"awesomeDSL/object"
+	"awesomeDSL/parser"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var Env = object.NewEnvironment()
+
+func GUI() {
+	p := tea.NewProgram(initialModel())
+
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type (
+	errMsg error
+)
+
+type model struct {
+	viewport    viewport.Model
+	messages    []string
+	textarea    textarea.Model
+	senderStyle lipgloss.Style
+	err         error
+}
+
+func initialModel() model {
+	ta := textarea.New()
+	ta.Placeholder = "Send a message..."
+	ta.Focus()
+
+	ta.Prompt = "┃ "
+	ta.CharLimit = 450
+
+	ta.SetWidth(90)
+	ta.SetHeight(3)
+
+	// Remove cursor line styling
+	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
+
+	ta.ShowLineNumbers = false
+
+	vp := viewport.New(130, 5)
+	vp.SetContent(`Welcome to the chat room!
+Type a message and press Enter to send.`)
+
+	ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	return model{
+		textarea:    ta,
+		messages:    []string{},
+		viewport:    vp,
+		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		err:         nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+	)
+
+	m.textarea, tiCmd = m.textarea.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+	//init
+	input := m.textarea.Value()
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			fmt.Println(m.textarea.Value())
+			return m, tea.Quit
+		case tea.KeyEnter:
+
+			m.messages = append(m.messages, m.senderStyle.Render("You: ")+m.textarea.Value())
+			m.viewport.SetContent(strings.Join(m.messages, "\n"))
+			m.textarea.Reset()
+			m.viewport.GotoBottom()
+			l := lexer.New(input)
+			p := parser.New(l)
+			program := p.ParseProgram()
+			if len(p.Errors()) != 0 {
+				m.err = fmt.Errorf("Woops! Something bad happens!\n parser errors:\n")
+				for _, msg := range p.Errors() {
+					m.err = fmt.Errorf("%s\t%s\n", m.err, msg)
+				}
+			}
+			evaluated := evaluator.Eval(program, Env)
+			if evaluated != nil {
+				m.messages = append(m.messages, m.senderStyle.Render("Bot: ")+evaluated.Inspect())
+				m.viewport.SetContent(strings.Join(m.messages, "\n"))
+				m.viewport.GotoBottom()
+			}
+		}
+
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	if m.err != nil {
+		return m, nil
+	}
+
+	return m, tea.Batch(tiCmd, vpCmd)
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"%s\n\n%s",
+		m.viewport.View(),
+		m.textarea.View(),
+	) + "\n\n"
+}
